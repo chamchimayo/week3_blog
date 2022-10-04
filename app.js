72fbbe67d1647ec46d024a8f725298dd3408c405
chamchimayo <chamchi.maayo@gmail.com>
2022-10-04 20:39:41 +0900

    feat: router 추가

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const app = express();
 const PORT = 3000;
+const router = require("./routes/index");
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
     res.send("메인 페이지입니다.")
 });
+
+app.use("/api", router);
 
 app.listen(PORT, () => {
     console.log(PORT, "서버가 실행되었습니다!");
